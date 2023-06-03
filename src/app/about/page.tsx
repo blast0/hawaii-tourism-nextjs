ac980e478058027b2d2a04715f382485ee6c7bcc
Bishal Kumar <Bishalk@attosol.com>
2023-06-04 01:08:56 +0530

    third  commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../navbar";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="Hero-section centerHV">
+      <Navbar />
+      <div className="highlights-text centerV">Highlights</div>
+    </div>
+  );
 }
 
 export default Page;
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -7,45 +7,59 @@ import o from "../assets/o.png";
 import h from "../assets/h.png";
 import a from "../assets/a.png";
 import menu from "../assets/menu.png";
+import close from "../assets/close.png";
+import Link from "next/link";
 
 export default function Navbar() {
   const [showSidemenu, setShowSidemenu] = useState(false);
   {
   }
   return (
-    <div className="nav-bar" onClick={() => setShowSidemenu(!showSidemenu)}>
+    <div className="nav-bar">
       {showSidemenu ? (
-        <div
-          className="leftmenu"
-          style={{
-            position: "absolute",
-            top: 0,
-            right: 0,
-            height: "100vh",
-            width: "365px",
-            backgroundColor: "white",
-          }}
-        ></div>
+        <div className="leftmenu IBM-400">
+          <Link href="/">
+            <div className="leftmenu-item">Home</div>
+          </Link>
+          <div className="leftmenu-item">Surfing</div>
+          <div className="leftmenu-item">Hula</div>
+          <div className="leftmenu-item">Vulcano</div>
+          <div className="menu-btn IBM-700">Book a trip</div>
+        </div>
       ) : (
         ""
       )}
       <div className="nav-bar-items centerV">
         <div className="menu-links centerV">
-          <div className="logo-black">
-            <Image src={A} width={23} height={33} alt="highlight image" />
-            <Image src={l} width={17} height={32} alt="highlight image" />
-            <Image src={o} width={17} height={21} alt="highlight image" />
-            <Image src={h} width={22} height={35} alt="highlight image" />
-            <Image src={a} width={23} height={21} alt="highlight image" />
-          </div>
+          <Link href="/">
+            <div className="logo-black">
+              <Image src={A} width={23} height={33} alt="highlight image" />
+              <Image src={l} width={17} height={32} alt="highlight image" />
+              <Image src={o} width={17} height={21} alt="highlight image" />
+              <Image src={h} width={22} height={35} alt="highlight image" />
+              <Image src={a} width={23} height={21} alt="highlight image" />
+            </div>
+          </Link>
           <div className="nav-bar-links IBM-400">
-            <div>Home</div>
+            <Link href="/">
+              <div>Home</div>
+            </Link>
             <div>Surfing</div>
             <div>Hula</div>
             <div>Vulcano</div>
           </div>
         </div>
         <div className="nav-bar-btn IBM-700 centerHV">Book a trip</div>
+      </div>
+      <div
+        className="sidemenu-toggle-icon hidden"
+        onClick={() => setShowSidemenu(!showSidemenu)}
+      >
+        {!showSidemenu ? (
+          <Image src={menu} width={30} height={20} alt="highlight image" />
+        ) : (
+          <Image src={close} width={24} height={24} alt="highlight image" />
+        )}
       </div>
     </div>
   );
